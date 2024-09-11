ef218193ddbf7d8301d2d0702df53c56b605f5d8
NotMacaco22 <casilva@uamv.edu.ni>
2024-09-10 18:44:33 -0600

    Adding Array

--- session7/src/main/java/org/cesar/Main.java
+++ session7/src/main/java/org/cesar/Main.java
@@ -2,6 +2,36 @@ package org.cesar;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] ages = new int[10];
+
+        ages[0] = 15;
+        ages[1] = 25;
+        ages[2] = 30;
+        ages[3] = 34;
+        ages[4] = 33;
+
+        int count = counter(ages);
+
+
+        for (int i = 0; i < count; i++){
+            System.out.print(ages[i] + " ");
+        }
+
+
     }
+
+    public static int counter(int array[]){
+
+        int count = 0;
+        /*
+        * By default elements are egual to 0, useless if there is actually an*/
+        for (int i : array){
+            if (i != 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
